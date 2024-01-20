db7948f83aae54a404dd0a83fc6b31f51704a91a
Hiroya Furusawa <pogbba.6@gmail.com>
2024-01-20 20:09:30 +0900

    useCallApiを追加

--- src/hooks/call-api-users/getUser.ts
+++ src/hooks/call-api-users/getUser.ts
@@ -1,30 +1,15 @@
-import { useState } from "react";
-import { AxiosError, isAxiosError } from "axios";
-import { usersRepository } from "@/repositories/users/repository.ts";
+import { useCallApi } from "@/hooks/callApi.ts";
 import { User } from "@/types/user.ts";
+import { usersRepository } from "@/repositories/users/repository.ts";
 import { convertUserParamsToData, UserParams } from "./converter.ts";
 
 export const useGetUser = () => {
     const { getUser } = usersRepository;
-
-    const [loading, setLoading] = useState(false);
-    const [user, setUser] = useState<User | null>(null);
-    const [error, setError] = useState<AxiosError | null>(null);
+    const { data: user, loading, error, exec } = useCallApi<User>();
 
     const fetchUser = async (params: UserParams) => {
-        setLoading(true);
-
-        try {
-            const paramsData = convertUserParamsToData(params);
-            const res = await getUser(paramsData);
-            setUser(res);
-        } catch (e) {
-            if (isAxiosError(e)) {
-                setError(e);
-            }
-        } finally {
-            setLoading(false);
-        }
+        const paramsData = convertUserParamsToData(params);
+        await exec(getUser(paramsData));
     };
 
     return {
